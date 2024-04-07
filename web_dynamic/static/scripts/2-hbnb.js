0bb1abdee00001dc5e41868af459b0c27e64293b
Shaker Sharabi <sheko100@icloud.com>
2024-04-07 17:38:36 +0200

    Add api status UI

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+$(function () {
+  const ameniNames = {};
+  $('.amenities ul input[type="checkbox"]').each(function () {
+    $(this).on('click', function (e) {
+      const checkbox = $(this);
+      const name = checkbox.parent().text();
+      if (checkbox.prop('checked')) {
+        ameniNames[name] = true;
+      } else {
+        delete ameniNames[name];
+      }
+      let ameniStr = '';
+      const ameniArr = Object.keys(ameniNames);
+      for (const name in ameniNames) {
+        ameniStr = ameniStr + name;
+        if (ameniArr.indexOf(name) + 1 !== ameniArr.length) {
+          ameniStr = ameniStr + ', ';
+        }
+      }
+      $('.amenities h4').text(ameniStr);
+    });
+  });
+  $.ajax('http://0.0.0.0:5001/api/v1/status/').done(function (api) {
+    if (api.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
